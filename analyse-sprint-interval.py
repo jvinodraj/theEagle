c5baf8e81ac5144cb561043130e873c766e8fc0c
Vinod <Vinodraj.j@gmail.com>
2025-02-19 17:46:05 +0530

    added cad, vr, hr, pace, and gct

--- analyse-sprint-interval.py
+++ analyse-sprint-interval.py
@@ -2,12 +2,14 @@ import pandas as pd
 import fitparse
 from pytz import timezone
 import datetime
+import pdb
 
 def load_df_fitparse(file_name):
     ist = timezone("Asia/Kolkata")
     recs  = fitparse.FitFile(file_name)
     data = []
     for record in recs.get_messages("record"):
+        # pdb.set_trace()
         data_dict = {field.name: field.value for field in record}
         # print(data_dict)
         data.append(data_dict)
@@ -20,23 +22,55 @@ def analyze_sprint_intervals(df, power_threshold=200, discard_interval_sec=30):
     """
     Analyze sprint intervals and calculate average power for each sprint.
 
-    :param df: Pandas DataFrame with 'timestamp' and 'power' columns.
+    :param df:              Pandas DataFrame with 'timestamp' and 'power' columns.
     :param power_threshold: Minimum power to consider as a sprint (adjust as needed).
+    :discard_interval_sec:  distacard any interval that is less than with partcular
+                            seconds
     """
-    sprint_intervals = []
-    in_sprint = False
-    current_sprint = []
+    sprint_intervals   = []
+    in_sprint          = False
+    current_sprint     = []
     timerange_interval = []
-
+    # pace
+    current_pace       = []
+    pace_interval      = []
+    # Cadence
+    current_cadence    = []
+    cadence_interval   = []
+    # Heart Rate
+    current_hr         = []
+    hr_interval        = []    
+    # stance_time
+    current_gct         = []
+    gct_interval        = []
+    # vertical_ratio
+    current_vr          = []
+    vr_interval         = []
 
     for i, row in df.iterrows():
+        # print(row['stance_time_balance'])
+        if row['stance_time_balance'] != None:
+            print(row['stance_time_balance'])
+            pdb.set_trace()
+
         if row['power'] >= power_threshold:
             if not in_sprint:
-                in_sprint = True
-                start_time = row['timezone_ist']
-                current_sprint = [row['power']]
+                in_sprint       = True
+                start_time      = row['timezone_ist']
+                current_pace    = [row['enhanced_speed']]
+                current_sprint  = [row['power']]
+                current_gct     = [row['stance_time']] 
+                current_cadence = [row['cadence'] * 2]
+                current_hr      = [row['heart_rate']]
+                current_vr      = [row['vertical_ratio']]
             else:
+                current_pace.append(row['enhanced_speed'])
                 current_sprint.append(row['power'])
+                current_gct.append(row['stance_time'])
+                current_cadence.append(row['cadence'] * 2)
+                current_hr.append(row['heart_rate'])
+                current_vr.append(row['vertical_ratio'])
+                
         else:
             if in_sprint:
                 in_sprint = False
@@ -46,20 +80,44 @@ def analyze_sprint_intervals(df, power_threshold=200, discard_interval_sec=30):
                 if (end_time - start_time).total_seconds() < discard_interval_sec:
                     continue
                 if current_sprint:
-                    sprint_intervals.append(current_sprint)
                     timerange_interval.append((start_time, end_time))
+                    sprint_intervals.append(current_sprint)
+                    pace_interval.append(current_pace)
+                    gct_interval.append(current_gct)
+                    cadence_interval.append(current_cadence)
+                    hr_interval.append(current_hr)
+                    vr_interval.append(current_vr)
 
     # Print average power for each sprint
     for i, sprint in enumerate(sprint_intervals):
         avg_power = sum(sprint) / len(sprint)
         start_time = timerange_interval[i][0].time()
         end_time   = timerange_interval[i][1].time()
-        print(f"Sprint {i+1}: Avg Power = {avg_power:.2f} Watt; between {start_time} and {end_time}")
+        # import pdb
+        # pdb.set_trace()
+        total_speed   = sum(pace_interval[i]) / len(pace_interval[i])
+        pace_min_per_km = (1000 / total_speed) / 60
+        
+        #calculating GCT
+        avg_gct = sum(gct_interval[i]) / len(gct_interval[i])
+
+        # avg cadence
+        avg_cad = sum(cadence_interval[i]) / len(cadence_interval[i])
+
+        #heart rate calculation
+        avg_hr  = sum(hr_interval[i]) / len(hr_interval[i])
+
+        #vertical ration 
+        avg_vr = sum(vr_interval[i]) / len(vr_interval[i])
+
+        print(f"Sprint {i+1}: [{start_time} - {end_time}] Avg Pace = {pace_min_per_km:.2f} min/km; \
+Avg Pow = {avg_power:.2f}w; GCT = {avg_gct:.2f} ms; Avg Cad = {avg_cad:.0f} SPM; \
+Avg HR = {avg_hr:.0f}; avg VR = {avg_vr:.2f} cm")
 
 
 
 if __name__ == "__main__":
-    file_name = r"C:\Users\a717631\OneDrive\Documents\Repo\theEagle\interval\18-Feb-2025.fit"
+    file_name = r"C:\Users\a717631\fits\interval\18-Feb-2025.fit"
     df = load_df_fitparse(file_name)
 
     # Run analysis
